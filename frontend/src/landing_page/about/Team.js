5090c1f7728aaacfbd41b555ee7fbda2984ccd1c
Your Name <your.email@example.com>
2025-04-17 21:33:12 +0530

    NEW

--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -13,7 +13,7 @@ function Team() {
         style={{ lineHeight: "1.8", fontSize: "1.2em" }}
       >
         <div className="col-6 p-3 text-center">
-            <img src="media/images/nithinKamath.jpg" style={{borderRadius:"100%",width:"50%"}}
+            <img src="/Media/images/nithinKamath.jpg" style={{borderRadius:"100%",width:"50%"}}
              />
              <h4 className="mt-5">Nithin Kamath</h4>
              <h6>Founder, CEO</h6>
